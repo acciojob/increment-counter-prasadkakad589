dcfc178f52859381e163905aac021c7d3b988f01
Prasad Kakad <prasadkakad118@gmail.com>
2026-03-18 06:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter=document.getElementById("counter");
+let count=0;
+function incrementCounter(){
+	alert(count);
+	count++;
+	counter.textContent=count;
+}
